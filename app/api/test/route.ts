5c464f95ce7c6db657c905df72bb4f446a1aeb3a
Evan Barras <evanbarras@mac.home>
2026-05-07 13:38:06 +0200

    chore: remove test endpoint

--- app/api/test/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-  const data = { test: 'ok', time: new Date().toISOString() }
-  const json = JSON.stringify(data)
-  console.log('Returning:', json)
-  return new Response(json, {
-    headers: { 'Content-Type': 'application/json' },
-  })
-}
